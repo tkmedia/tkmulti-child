e29900090a1a3830a0e5dee314d2fe4a1b93c112
Tal Katz <tal@tkmedia.co.il>
2019-07-07 14:05:07 +0300

    Update class-mfn-tgmpa.php

--- inc/class-mfn-tgmpa.php
+++ inc/class-mfn-tgmpa.php
@@ -171,7 +171,7 @@ if( ! class_exists( 'TGM_Plugin_Activation' ) ){
 			'id'           => 'tkmulti',                 // Unique ID for hashing notices for multiple instances of TGMPA.
 			'default_path' => '',                      // Default absolute path to bundled plugins.
 			'menu'         => 'tgmpa-install-plugins', // Menu slug.
-			'parent_slug'  => 'tkmulti.php',            // Parent menu slug.
+			'parent_slug'  => 'themes.php',            // Parent menu slug.
 			'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
 			'has_notices'  => true,                    // Show admin notices or not.
 			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.

8a1c849c8fd36c7da76cd4f5db71e60d3afa71d3
Tal Katz <tal@tkmedia.co.il>
2019-07-14 22:44:07 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,15 +9,22 @@ function parent_theme_field_groups($paths) {
 
 function tkmulti_child_add_stylesheet() {
     wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/css/custom-style.css', false, '1.0', 'all' );
+	// Google Fonts ----------------------------------------------------------
+	wp_enqueue_style( 'tkmulti-fonts', tkmulti_theme_fonts_url() );
 }
 add_action( 'wp_enqueue_scripts', 'tkmulti_child_add_stylesheet', 999 );
 
-function tkmulti_child_google_fonts() { 
-	wp_enqueue_style('tkmulti_fonts_Assistant', 'https://fonts.googleapis.com/css?family=Heebo:400,500,700&display=swap&subset=hebrew'); 
-}
-add_action( 'wp_enqueue_scripts', 'tkmulti_child_google_fonts' ); 
-
-// TGM ------------------------------------------------------------------
-if( is_admin() ){
-	include_once get_stylesheet_directory_uri() .'/inc/class-mfn-tgmpa.php';
+/* ---------------------------------------------------------------------------
+ * Theme Fonts URL
+ * --------------------------------------------------------------------------- */
+function tkmulti_theme_fonts_url() {
+	$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Montserrat:300,400,500,600,700,800&display=swap' ) );
+	//$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Assistant:200,400,600,700' ) );
+	//$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Raleway:300,400,600,700,800&display=swap&subset=latin-ext' ) );
+	$query_args = array(
+		'family' => implode( '|', $font_families ),
+		'subset' => 'hebrew,latin',
+	);
+	$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	return esc_url_raw( $fonts_url );
 }

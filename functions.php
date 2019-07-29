3c12d35b8ee6a1f781eea8f8457abd9bc6aaae3b
Tal TKMedia MacBook 2017 <tal@tkmedia.co.il>
2019-07-29 09:34:09 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,24 +7,15 @@ function parent_theme_field_groups($paths) {
   return $paths;
 }
 
-function tkmulti_child_add_stylesheet() {
-    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/css/custom-style.css', false, '1.0', 'all' );
-	// Google Fonts ----------------------------------------------------------
-	wp_enqueue_style( 'tkmulti-fonts', tkmulti_theme_fonts_url() );
+add_action( 'wp_enqueue_scripts', 'my_plugin_add_stylesheet' );
+function my_plugin_add_stylesheet() {
+    wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/css/theme-style.css', false, '1.0', 'all' );
 }
-add_action( 'wp_enqueue_scripts', 'tkmulti_child_add_stylesheet', 999 );
 
 /* ---------------------------------------------------------------------------
  * Theme Fonts URL
  * --------------------------------------------------------------------------- */
 function tkmulti_theme_fonts_url() {
-	$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Montserrat:300,400,500,600,700,800&display=swap' ) );
-	//$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Assistant:200,400,600,700' ) );
-	//$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Raleway:300,400,600,700,800&display=swap&subset=latin-ext' ) );
-	$query_args = array(
-		'family' => implode( '|', $font_families ),
-		'subset' => 'hebrew,latin',
-	);
-	$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
-	return esc_url_raw( $fonts_url );
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Assistant:400,600,700,800&display=swap&subset=hebrew', false ); 
 }
+ add_action( 'wp_enqueue_scripts', 'tkmulti_theme_fonts_url' );
